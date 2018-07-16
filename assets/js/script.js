f904251e5b6ebcfcf45a7fc3c43e84aad5562257
Lucas Cunha <you@example.com>
2018-07-16 18:43:15 -0300

    animação js adicionada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-document.querySelector('.nav-links')
+const titulo = document.querySelector(".descricao-header > h1 ");
+const subtitulo = document.querySelector(".descricao-header > p ");
+
+digitarTexto( titulo );
+setTimeout( function(){
+    digitarTexto( subtitulo );
+}, 1200)
+
+function digitarTexto( elemento ) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 110 * i)
+    });
+}
